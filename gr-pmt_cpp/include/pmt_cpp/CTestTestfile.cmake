14e793b191f3a78403659d322612833d287c8ce7
GrubiCom <GrubiCom@github.com>
2017-04-26 12:34:40 -0300

    ¨final¨

--- gr-pmt_cpp/include/pmt_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ariel/Documentos/GNURADIO/Blocks_trunk/gr-pmt_cpp/include/pmt_cpp
-# Build directory: /home/ariel/Documentos/GNURADIO/Blocks_trunk/gr-pmt_cpp/include/pmt_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
